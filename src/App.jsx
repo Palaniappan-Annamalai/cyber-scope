583f2e4b828bc588527756e6bbbb153b85fb81a3
pa527 <pa527@students.waikato.ac.nz>
2026-06-16 14:58:37 +0530

    Added Cyber Scope Visualization React Project

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
     <QueryClientProvider client={queryClientInstance}>
-      <BrowserRouter>
+      <BrowserRouter basename="/cyber-scope">
         <ScrollToTop />
         <Layout>
           <Routes>
